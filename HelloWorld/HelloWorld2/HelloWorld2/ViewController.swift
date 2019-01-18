684ab3346c0bd6294d39874482a54a10162f37c8
akaminedaisuke <46768197+akaminedaisuke@users.noreply.github.com>
2019-01-18 18:59:52 +0900

    daisukeTask HelloWorld

---
+++ HelloWorld/HelloWorld2/HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by USER on 2019/01/18.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
