784b7ef6121aa8645bb268bd2e8e1a75dbc4eb0d
akaminedaisuke <daisuke.akamine@n1system.com>
2019-01-23 16:16:01 +0900

    2019.1.23

--- HelloWorld/HelloWorld2/HelloWorld2/ViewController.swift
+++ HelloWorld/HelloWorld2/HelloWorld2/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
     }
+    @IBOutlet var HelloWorld: UILabel!
     @IBOutlet var mainImage: UIImageView!
     @IBAction func kakusu(_ sender: Any) {
         mainImage.isHidden = true;
     }
     @IBAction func hyouji(_ sender: Any) {
         mainImage.isHidden = false;
-        
     }
 }
