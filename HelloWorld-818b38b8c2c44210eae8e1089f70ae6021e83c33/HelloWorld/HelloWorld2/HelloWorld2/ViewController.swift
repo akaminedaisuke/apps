c09fc8f54a3aaa700169e4904fd6c84424c1c4b6
akaminedaisuke <46768197+akaminedaisuke@users.noreply.github.com>
2019-01-23 17:13:17 +0900

    Add files via upload

---
+++ HelloWorld-818b38b8c2c44210eae8e1089f70ae6021e83c33/HelloWorld/HelloWorld2/HelloWorld2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by USER on 2019/01/18.
+//  Copyright © 2019 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet var HelloWorld: UILabel!
+    @IBOutlet var mainImage: UIImageView!
+    @IBAction func kakusu(_ sender: Any) {
+        mainImage.isHidden = true;
+    }
+    @IBAction func hyouji(_ sender: Any) {
+        mainImage.isHidden = false;
+    }
+}
