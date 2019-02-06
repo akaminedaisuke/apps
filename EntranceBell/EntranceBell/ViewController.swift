a7aba92f0fbc0eff64cd88df7730723e157201ae
akaminedaisuke <46768197+akaminedaisuke@users.noreply.github.com>
2019-02-06 17:23:15 +0900

    EntranceBell(ver1.3.0)

--- EntranceBell/EntranceBell/ViewController.swift
+++ EntranceBell/EntranceBell/ViewController.swift
@@ -11,11 +11,21 @@ import AVFoundation
 class ViewController: UIViewController {
     @IBOutlet var CoLogo: UIImageView!
     @IBOutlet var mainMessage: UILabel!
+    @IBOutlet var button: UIButton!
+    var lineBreakMode:NSLineBreakMode!
     var audioPlayer : AVAudioPlayer! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Do any additional setup after loading the view, typically from a nib.
+        //ボタンの設定
+        let rgba =  UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+        button.backgroundColor = rgba
+        button.layer.borderWidth = 2.0
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 50.0
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        //音声を再生するための設定
         let soundFilePath = Bundle.main.path(forResource: "info-lady1_info-lady1-shibarakuomachi1",ofType:"mp3")!
         let sound:URL = URL(fileURLWithPath: soundFilePath)
         do{
@@ -25,20 +35,17 @@ class ViewController: UIViewController {
         }
         audioPlayer.prepareToPlay()
     }
-    
+    //呼び出し（Touchボタン）のアクション
     @IBAction func Touch(_ sender: Any) {
         audioPlayer.currentTime = 0
         audioPlayer.play()
         mainMessage.text = "担当者が参ります 暫くお待ち下さい"
     }
-    
+    //Returnボタンのアクション
     @IBAction func Return(_ sender: Any) {
-        mainMessage.text = "ご来社ありがとうございます。ようこそエヌワンへ！ご用の方は下記のボタンにタッチして下さい。"
+        mainMessage.text = "ご来社ありがとうございます。ようこそエヌワンへ！\nご用の方は下のボタンにタッチして下さい。"
     }
-    
-        override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
 }
-
-
